50fa2c21938336fbc3d06c8f70634be8cd4039df
Madi <madi_gaziz@mail.ru>
2024-01-21 15:21:27 +0600

    Made migrations and added Message and Chat classes to the admin panel

---
+++ app_messanger/models/chat.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+from .message import Message
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chats')
+    message = models.ManyToManyField(Message, related_name='chats')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Chat with ID {self.id} create at {self.created_at}'
