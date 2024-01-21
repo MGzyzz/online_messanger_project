930e0ae82923947c241dea66c82e2100a2c9119d
Madi <madi_gaziz@mail.ru>
2024-01-21 22:29:39 +0600

    Connected the project to the postgresql database

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_messanger',
+        'USER': 'admin',
+        'PASSWORD': 'admin_root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
